aa388c15a1cba4821bcf1d72cec5c3834f0be75f
divyanshuraj1095 <divyanshuraj23102005@gmail.com>
2025-04-12 03:25:15 +0530

    register user

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-
+import { app } from "./app.js";
 import dotenv from "dotenv"
 
 import connectDB from "./db/index.js";
@@ -9,7 +9,7 @@ dotenv.config({
 
 connectDB()
 .then(()=>{
-    app.listen(process.env.PORT || 8000, ()=>{
+     app.listen(process.env.PORT || 8000, ()=>{
      console.log(`Server running on the port : ${process.env.PORT}`)
     })
 })
